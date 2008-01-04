0790568be9b287f79a85c1fecf84b0703f583f8f
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-01-04 22:47:31 +0000

    wrapping it in SMT module, for namespace conflicts
    git-svn-id: http://thmadb.com/public_svn/plugins/smt_active_record_extensions@234 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/active_record_extensions/grouped_find.rb
+++ lib/active_record_extensions/grouped_find.rb
@@ -1,23 +1,25 @@
-module ActiveRecordExtensions
-  module GroupedFind
-    # Using find(:all).each is often terrible inefficient.  It's a memory
-    # hog.  Usually finding the records in groups, and then iterating through
-    # those groups is a better solution.
-    #
-    # This wraps ActiveRecord::Base#find(:all), yielding each record as if
-    # you had used find(:all).each { |record| }
-    def find_in_groups_of(num, hash={})
-        number_of_items = self.count
-      offset = 0
-      
-      while number_of_items > 0
-        self.find(:all, hash.merge(:limit => num, :offset => offset)).each do |record|
-          yield record
+module SMT
+  module ActiveRecordExtensions
+    module GroupedFind
+      # Using find(:all).each is often terrible inefficient.  It's a memory
+      # hog.  Usually finding the records in groups, and then iterating through
+      # those groups is a better solution.
+      #
+      # This wraps ActiveRecord::Base#find(:all), yielding each record as if
+      # you had used find(:all).each { |record| }
+      def find_in_groups_of(num, hash={})
+          number_of_items = self.count
+        offset = 0
+
+        while number_of_items > 0
+          self.find(:all, hash.merge(:limit => num, :offset => offset)).each do |record|
+            yield record
+          end
+
+          number_of_items -= num
+          offset += num
         end
-        
-        number_of_items -= num
-        offset += num
       end
     end
-  end
+  end  
 end
