4f1c377255561faae56e6f6b91855f92520c3308
smt <smt@6dbbdfbd-06da-443d-8068-b1bdd22a71ef>
2008-01-25 17:33:37 +0000

    comments
    git-svn-id: http://thmadb.com/public_svn/plugins/smt_active_record_extensions@297 6dbbdfbd-06da-443d-8068-b1bdd22a71ef

--- lib/smt/active_record_extensions/query_combiner.rb
+++ lib/smt/active_record_extensions/query_combiner.rb
@@ -1,5 +1,19 @@
 module SMT
   module ActiveRecordExtensions
+    # I'm an ActiveRecord Query Combiner.  I Combine fragments of sql arrays into new sql arrays, like so:
+    #
+    #   a1 = QueryCombiner.new ["a = ? or b = ?", 7, 9]
+    #   a2 = QueryCombiner.new ["c = ?", 10]
+    #   a1 & a2 => ["(a = ? or b = ?) AND c = ?", 7, 9, 10]
+    # 
+    # I'm especially helpful when SQL WHERE clauses need to be joined dynamically in Rails Apps.
+    #
+    # My "&" and "|" operations are left-associative.  They bind more tightly than my "and" and "or" operators
+    # (just like ruby's && and || bind more tightly than "and" and "or").  The "&" and "|" operators wrap
+    # their arguments in parenthesis, the "and" and "or" are a more simple concatination.
+    #
+    # These operators return a new QueryCombiner, but the QueryCombiner is a kind of Array, and can be treated
+    # as such.
     class QueryCombiner < Array
       def &(other)
         QueryCombiner.new ["(#{query_string}) AND (#{other.query_string})", *combine_values(other)]
