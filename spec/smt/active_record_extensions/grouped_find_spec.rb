ae1d781f935c6ad707542236d77546ed106bffa5
Scott Taylor <scott@railsnewbie.com>
2008-10-11 21:56:43 -0400

    * Refactor specs

--- spec/smt/active_record_extensions/grouped_find_spec.rb
+++ spec/smt/active_record_extensions/grouped_find_spec.rb
@@ -1,23 +1,5 @@
 require File.dirname(__FILE__) + "/../../spec_helper"
 
-require 'rubygems'
-require 'sqlite3'
-require 'active_record'
-require 'active_support'
-
-ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
-ActiveRecord::Migration.verbose = false
-
-ActiveRecord::Schema.define do
-  create_table :users, :force => true do |t|
-    t.string :first_name
-    t.string :last_name
-    t.timestamps
-  end
-end
-
-class User < ActiveRecord::Base; end
-
 module SMT
   module ActiveRecordExtensions
     describe GroupedFind do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,21 @@
 require 'rubygems'
 require 'active_record'
 require File.dirname(__FILE__) + "/../lib/smt"
+
+require 'rubygems'
+require 'sqlite3'
+require 'active_record'
+require 'active_support'
+
+ActiveRecord::Base.establish_connection :adapter => 'sqlite3', :database  => ':memory:'
+ActiveRecord::Migration.verbose = false
+
+ActiveRecord::Schema.define do
+  create_table :users, :force => true do |t|
+    t.string :first_name
+    t.string :last_name
+    t.timestamps
+  end
+end
+
+class User < ActiveRecord::Base; end
